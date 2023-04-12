f96005888405f332ae82f84c63c3f2e848296a83
JungMunGyu <sii9059@naver.com>
2023-04-12 15:23:29 +0900

    [1장] JSX 문법 및 실행확인

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = 'MunGyu';
+  const add = (a, b) => a+b;
+  const isFullname = false;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>1 + 2 = {add(1, 2)}</Text>
+      <Text>{isFullname === true ? name + ' Jung' : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
