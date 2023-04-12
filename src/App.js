ff828139f771e188d5ecaf90201a595546321963
JungMunGyu <sii9059@naver.com>
2023-04-12 18:08:36 +0900

    [2장] useState를 사용하여 더하기 빼기 구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Button, { ButtonTypes } from './components/Button';
+import { useState } from 'react';
 export default function App() {
-  // 1장 내용
-  //   const name = 'MunGyu';
-  //   const add = (a, b) => a + b;
-  //   const isFullname = false;
-  //   <Text>1 + 2 = {add(1, 2)}</Text>
-  //   <Text>{isFullname === true ? name + ' Jung' : name}</Text>
-
-  //2장 - 스타일
-  //   {/* <Text style={styles.error}>Error Message</Text>
-  //       {/* 여러개의 스타일 시트 전달 -> 배열 형태 , 뒤에가 앞을 덮어쓴다*/}
-  //       <Text style={[styles.text, styles.error]}>Error Message</Text>
-  //       <Text style={[styles.error, styles.text]}>Error Message</Text>
-  //       {/* 조건에 따라 에러가 있을때 에러메세지 출력 */}
-  //       <Text style={[styles.text, isError && styles.error]}>Error Message</Text>
-
+  const [result, setResult] = useState(0);
   return (
     <View style={styles.container}>
+      <Text style={{ fontSize: 60 }}> {result}</Text>
       <Button
-        title="1"
-        onPress={() => {}}
-        buttonStyle={{ width: 100, height: 100 }}
-        buttonType={ButtonTypes.NUMBER}
+        title="+"
+        onPress={() => {
+          setResult(result + 1);
+        }}
+        buttonStyle={{ width: 100, height: 100, marginBottom: 10 }}
+        buttonType={ButtonTypes.OPERATOR}
       />
       <Button
-        title="0"
-        onPress={() => {}}
-        buttonStyle={{ width: 200, height: 100 }}
-      />
-      <Button
-        title="="
-        onPress={() => {}}
+        title="-"
+        onPress={() => {
+          setResult(result - 1);
+        }}
         buttonStyle={{ width: 100, height: 100 }}
         buttonType={ButtonTypes.OPERATOR}
       />
@@ -60,3 +47,18 @@ const styles = StyleSheet.create({
     color: 'red',
   },
 });
+
+// 1장 내용
+//   const name = 'MunGyu';
+//   const add = (a, b) => a + b;
+//   const isFullname = false;
+//   <Text>1 + 2 = {add(1, 2)}</Text>
+//   <Text>{isFullname === true ? name + ' Jung' : name}</Text>
+
+//2장 - 스타일
+//   {/* <Text style={styles.error}>Error Message</Text>
+//       {/* 여러개의 스타일 시트 전달 -> 배열 형태 , 뒤에가 앞을 덮어쓴다*/}
+//       <Text style={[styles.text, styles.error]}>Error Message</Text>
+//       <Text style={[styles.error, styles.text]}>Error Message</Text>
+//       {/* 조건에 따라 에러가 있을때 에러메세지 출력 */}
+//       <Text style={[styles.text, isError && styles.error]}>Error Message</Text>
