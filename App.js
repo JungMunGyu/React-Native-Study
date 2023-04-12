0fb15acb31f81964c4f59d959ec325a96cb1fe09
JungMunGyu <sii9059@naver.com>
2023-04-12 16:14:44 +0900

    [2장] 컴포넌트와 스타일

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default function App() {
-  const name = 'MunGyu';
-  const add = (a, b) => a+b;
-  const isFullname = false;
-
-  return (
-    <View style={styles.container}>
-      <Text>1 + 2 = {add(1, 2)}</Text>
-      <Text>{isFullname === true ? name + ' Jung' : name}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  // 1장 내용
+  //   const name = 'MunGyu';
+  //   const add = (a, b) => a + b;
+  //   const isFullname = false;
+  //   <Text>1 + 2 = {add(1, 2)}</Text>
+  //   <Text>{isFullname === true ? name + ' Jung' : name}</Text>
+  const isError = true;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>RN Calc App</Text>
+      <Text style={styles.error}>Error Message</Text>
+      {/* 여러개의 스타일 시트 전달 -> 배열 형태 , 뒤에가 앞을 덮어쓴다*/}
+      <Text style={[styles.text, styles.error]}>Error Message</Text>
+      <Text style={[styles.error, styles.text]}>Error Message</Text>
+      {/* 조건에 따라 에러가 있을때 에러메세지 출력 */}
+      <Text style={[styles.text, isError && styles.error]}>Error Message</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+//스타일 시트로 객체로 스타일 전달
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'green',
+  },
+  error: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: 'red',
+  },
+});
